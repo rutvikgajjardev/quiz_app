015614ff03fc73ad6f8ee74595e2be61b2c64768
Rutvik Gajjar <rutvikgajjardev@gmail.com>
2023-06-12 19:25:02 +0530

    initial commit

---
+++ lib/questions_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuistionsScreenState();
+  }
+}
+
+class _QuistionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'ques',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'Questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'Questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 17, 2, 234),
+                Color.fromRGBO(0, 14, 138, 1),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
